1058b27fa7e3656d8b883da9072caef312199274
tha <tha@CPH-R9039Y2P.efif.dk>
2015-06-02 10:55:33 +0200

    One more class added

---
+++ src/gittest/SomeClass.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest;
+
+/**
+ *
+ * @author tha
+ */
+public class SomeClass {
+    String someField;
+    int someOtherField;
+
+    public String getSomeField() {
+        return someField;
+    }
+
+    public void setSomeField(String someField) {
+        this.someField = someField;
+    }
+
+    public int getSomeOtherField() {
+        return someOtherField;
+    }
+
+    public void setSomeOtherField(int someOtherField) {
+        this.someOtherField = someOtherField;
+    }
+    
+}
